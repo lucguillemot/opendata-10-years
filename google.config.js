dcffc129858cb780851044bf27e8832d853f92bb
lucguillemot <luc.guillemot@gmail.com>
2022-04-05 17:14:33 +0200

    feat: update google config

--- google.config.js
+++ google.config.js
@@ -1,11 +1,6 @@
 export default [
-	{
-		"id": "1352iFuTSDDFPNAXBbOaEXkF8mCdjPu6B43hpUF2P3C4",
-		"filepath": "src/data/doc.json"
-	},
-	{
-		"id": "1te65h_nywgXVAzvV-9E5fmHGblcKvctf_cNZgeam9Tk",
-		"gid": "0",
-		"filepath": "src/data/sheet.csv"
-	}
-]
+  {
+    id: "1PE5M-3p8gQuXCL54RczEoDNZDnjX2lHJglw2qSxwZl4",
+    filepath: "src/data/doc.json"
+  }
+];
