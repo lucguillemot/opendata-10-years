94b104138e40eeba1d1c764c66d12bbd976a9196
lucguillemot <luc.guillemot@gmail.com>
2022-04-05 16:13:20 +0200

    fix: remove pudding things

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,9 +1,3 @@
 <script>
-  import Demo from "$components/demo/Demo.svelte";
-  import WIP from "$components/helpers/WIP.svelte";
-  // import Footer from "$components/Footer.svelte";
 </script>
 
-<WIP />
-<Demo />
-<!-- <Footer /> -->
