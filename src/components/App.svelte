a41d250a23d12b7d1c195b7c5c05a9f8b8c54545
lucguillemot <luc.guillemot@gmail.com>
2022-04-07 09:38:20 +0200

    feat: add intro

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -6,6 +6,7 @@
 
 <Section>
   <h1>{@html doc.title}</h1>
+  <p>{@html doc.introduction}</p>
 </Section>
 
 <Years />
@@ -15,5 +16,14 @@
     font-family: "m3regular";
     font-size: 5.625rem;
     line-height: 1.2;
+    text-align: center;
+  }
+  p {
+    font-family: "m3light";
+    font-size: 2rem;
+    line-height: 1.4;
+    font-weight: 300;
+    text-align: center;
+    margin: 3rem 0;
   }
 </style>
