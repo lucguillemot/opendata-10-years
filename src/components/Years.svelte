7dd2311d5def9cb5caa090508d6b6c207ea0e758
lucguillemot <luc.guillemot@gmail.com>
2022-04-21 10:46:34 +0200

    feat: add loading indicator

--- src/components/Years.svelte
+++ src/components/Years.svelte
@@ -8,4 +8,45 @@
   });
 </script>
 
-<svelte:component this={Scrolly} />
+<div class="wrapper">
+  {#if !Scrolly}
+    <div class="loader" />
+  {:else}
+    <svelte:component this={Scrolly} />
+  {/if}
+</div>
+
+<style>
+  .wrapper {
+    position: relative;
+    width: 100%;
+    min-height: 150px;
+    display: flex;
+    justify-content: center;
+  }
+  .loader {
+    border: 8px solid #f3f3f3;
+    border-radius: 50%;
+    border-top: 8px solid #bbb;
+    width: 100px;
+    height: 100px;
+    -webkit-animation: spin 2s linear infinite; /* Safari */
+    animation: spin 2s linear infinite;
+  }
+  @-webkit-keyframes spin {
+    0% {
+      -webkit-transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
